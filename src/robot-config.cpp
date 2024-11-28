237c8db3732d6f364df21527aab704b50b91d174
zactang1231 <ztang28@knox.nsw.edu.au>
2024-11-28 21:25:18 +1100

    Tune LB

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -42,7 +42,7 @@ float verticalOffset = -0.75;
 
 char acceptColour;
 
-float LBTargetPos = 0.0;
+float LBTargetPos = 210;
 
 // controller
 pros::Controller controller1(pros::E_CONTROLLER_MASTER);
@@ -179,16 +179,26 @@ void prerollerStop() {
 void handleLBStateDown() {
     if (LBState == SCORING) {
         LBState = DEFAULT; // Go to default if scoring 
+        controller1.print (0,0,"scoring-default");
     } else if (LBState == DEFAULT) {
-        LBState= LOADING; // Go to loading if default
+        LBState = LOADING; // Go to loading if default
+        controller1.print (0,0,"default-loading");
+    } else if (LBState == LOADING) {
+        // Stay in LOADING if already there
+        controller1.print (0,0,"loading");
     }
 }
 
 void handleLBStateUp() {
     if (LBState == LOADING) {
         LBState = DEFAULT; // Go to default if loading 
+        controller1.print (0,0,"loading-default");
     } else if (LBState == DEFAULT) {
-        LBState= SCORING; // Go to scoring if default
+        LBState = SCORING; // Go to scoring if default
+        controller1.print (0,0,"default-scoring");
+    } else if (LBState == SCORING) {
+        // Stay in SCORING if already there
+        controller1.print (0,0,"scoring");
     }
 }
 
@@ -197,19 +207,18 @@ void updateLBMotor() {
 
     switch (LBState) {
         case LOADING:
-            lb.move_absolute(0, 100);
+            // lb.move_absolute(0, 100);
             LBTargetPos = 0;
             std::cout << "Lady Brown: Scoring" << std::endl;
             break;
         case DEFAULT:
-        default:
-            lb.move_absolute(-210, 100);
-            LBTargetPos = 210;
+            // lb.move_absolute(-210, 100);
+            LBTargetPos = -210;
             std::cout << "Lady Brown: Default" << std::endl;
             break;
         case SCORING:
-            lb.move_absolute(-270, 100);
-            LBTargetPos = 270;
+            // lb.move_absolute(-360, 100);
+            LBTargetPos = -390;
             std::cout << "Lady Brown: Loading" << std::endl;
             break;
     }
@@ -218,14 +227,18 @@ void updateLBMotor() {
 }
 
 void LBSpinToTarget() {
-    if (controller1.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
-        handleLBStateUp();
-    } else if (controller1.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
-        handleLBStateDown();
+    while (true) {
+        if (controller1.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
+            handleLBStateUp();
+        } else if (controller1.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+            handleLBStateDown();
+        }
+        updateLBMotor();
+        lb.move_absolute(LBTargetPos, 70);
+
+        // delay so the Brain doesn't explode
+        pros::delay(25);
     }
-    handleLBState();
-    updateLBMotor();
-    lb.move_absolute(-LBTargetPos, 100);
 }
 
 void handleUptakeState() {
