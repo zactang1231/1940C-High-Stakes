16aa3d3643dc4e4d3353f559f4dc38a797b5d658
zactang1231 <ztang28@knox.nsw.edu.au>
2024-12-01 13:26:17 +1100

    Added redpos

--- src/autons/redpos.cpp
+++ src/autons/redpos.cpp
@@ -22,24 +22,24 @@
 // get a path used for pure pursuit
 // this needs to be put outside a function
 // '.' is replaced with "_" to make c++ happy
-ASSET(bluepos1_txt);
+ASSET(redpos1_txt);
 
-void bluepos() {
+void redpos() {
     preroller.move(127);
     uptake.move(127);
     handleLBStateUp();
     mogo.set_value(false);
 
-    chassis.setPose(61, -33, 90);
+    chassis.setPose(-61, -33, 90);
 
     // First path
-    chassis.follow(bluepos1_txt, 15, 10000, false);
+    chassis.follow(redpos1_txt, 15, 10000, false);
     mogo.set_value(true);
     // Rings
-    chassis.moveToPose(23.5, -23.5, 180, 4000);
-    chassis.moveToPose(23.5, -47, 180, 4000);
+    chassis.moveToPose(-23.5, -23.5, 180, 4000);
+    chassis.moveToPose(-23.5, -47, 180, 4000);
     // Mogo
-    chassis.moveToPose(13.6, -55, 270, 4000);
+    chassis.moveToPose(-13.6, -55, 270, 4000);
     doinker.set_value(true);
-    chassis.moveToPose(35, -55, 270, 4000, {.forwards = false});
+    chassis.moveToPose(-35, -55, 270, 4000, {.forwards = false});
 }   
